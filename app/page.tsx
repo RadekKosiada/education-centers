159fb965b81f508cf5cdd727cfde50482fb7cb7f
Radoslaw Kosiada <kosiada@skygate.de>
2026-02-23 14:29:23 +0100

    add simple fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 
-export default function Home() {
+export default async function Home() {
+    const URL = "https://www.vhsit.berlin.de/VHSKURSE/OpenData/Kurse.json";
+    const data = await fetch(URL);
+    const courses = await data.json();
+    console.log('COURSES: ', courses);
+    
     return (
         <div className="">
             <main className="">
