7de24da587e3207d7c2d33146ca4696d18629ff4
RadekKosiada <kosiadar@gmail.com>
2026-02-26 10:48:58 +0100

    handle cache max size problem

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheHandler:
+    require.resolve("next/dist/server/lib/incremental-cache/file-system-cache.js"),
 };
 
 export default nextConfig;
