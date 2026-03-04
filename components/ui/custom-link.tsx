c24855df5ba78cb85f8a256768f9862d89f3b30e
RadekKosiada <kosiadar@gmail.com>
2026-03-04 16:27:02 +0100

    add custom-link for A11y

---
+++ components/ui/custom-link.tsx
@@ -0,0 +1,33 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { type VariantProps } from "class-variance-authority";
+import Link, { LinkProps } from 'next/link';
+import { HTMLProps } from 'react';
+
+
+export function CustomLink({
+    className,
+    variant,
+    replace,
+    scroll,
+    prefetch,
+    onNavigate,
+    size,
+    href,
+    children,
+    ...props
+}: LinkProps & HTMLProps<HTMLAnchorElement> & VariantProps<typeof buttonVariants>) {
+    return (
+        <Link
+            href={href}
+            className={cn(buttonVariants({ variant, size, className }), "underline")}
+            replace={replace}
+            scroll={scroll}
+            prefetch={prefetch}
+            onNavigate={onNavigate}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
+};
