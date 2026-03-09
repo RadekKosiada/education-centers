936e7c3cec4c0b85f8a2b9c3a6f909895ef67da4
RadekKosiada <kosiadar@gmail.com>
2026-03-09 16:06:04 +0100

    add Loading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { CompactTable } from "@/components/compact-table";
 import { NoCoursesError } from "@/components/no-courses-error";
+import { Suspense } from 'react';
 import { loadCourses } from "./lib/load-courses";
+import Loading from "./loading";
 
 export default async function Home() {
 
@@ -16,10 +18,12 @@ export default async function Home() {
 
     // If no error, render normally    
     return (
-        <div className="">
-            <main className="">
-                <CompactTable courses={courses} />
-            </main>
-        </div>
+        <Suspense fallback={<Loading />}>
+            <div className="">
+                <main className="">
+                    <CompactTable courses={courses} />
+                </main>
+            </div>
+        </Suspense>
     );
 }
