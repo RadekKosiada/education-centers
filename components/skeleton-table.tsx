2b94af21ae2f160e3c19daafa3e241d21ba5c9aa
RadekKosiada <kosiadar@gmail.com>
2026-03-10 12:01:05 +0100

    adjust skeleton-table

--- components/skeleton-table.tsx
+++ components/skeleton-table.tsx
@@ -4,6 +4,7 @@ import { rowsPerPage, tableHeadsLabels } from "@/lib/table-categories";
 export function SkeletonTable() {
     return (
         <div className="flex w-full flex-col gap-2">
+            <Skeleton className="h-8 flex-1 py-2 mt-4" />
             {Array.from({ length: rowsPerPage }).map((_, index) => (
                 <div className="flex gap-4" key={index}>
                     {tableHeadsLabels.map(label => (
