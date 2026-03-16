48b59ee58cedfb757e2a40b31590cf4768d47d0d
RadekKosiada <kosiadar@gmail.com>
2026-03-16 15:24:14 +0100

    adjust border for table header

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -23,7 +23,7 @@ function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
     return (
         <thead
             data-slot="table-header"
-            className={cn("[&_tr]:border-b", className)}
+            className={cn("[&_tr]:border-b border-b-2 border-primary", className)}
             {...props}
         />
     )
