269d92edd49ec410c4c37454f17435c8d7f22868
RadekKosiada <kosiadar@gmail.com>
2026-03-19 11:52:45 +0100

    move search functionality to table-wrapper

--- components/table-wrapper.tsx
+++ components/table-wrapper.tsx
@@ -3,15 +3,40 @@
 import { useSearchParams } from 'next/navigation';
 
 import { CompactTable } from "@/components/compact-table";
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { SearchInput } from './search-input';
 
 export function TableWrapper({ courses }: { courses: Array<any> }) {
 
+    const pathname = usePathname();
+    const router = useRouter();
+    const [searchInputValue, setSearchInputValue] = useState('');
+
     const searchParams = useSearchParams();
     const searchQuery = searchParams && searchParams.get('search');
 
     const [filteredCourses, setFilteredCourses] = useState(courses);
+
+    const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInputValue(e.target.value);
+    };
+
+    const handleRouter = () => {
+        if (searchInputValue) return router.push(`${pathname}?search=${searchInputValue}`);
+        if (!searchInputValue) return router.push('/');
+    };
+
+    useEffect(() => {
+        handleRouter();
+    }, [searchInputValue]);
+
+    const handleSearchInputKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+        if (e.key === 'Enter') {
+            handleRouter();
+        }
+    };
+
 
     useEffect(() => {
         const handleSearch = () => {
@@ -39,7 +64,7 @@ export function TableWrapper({ courses }: { courses: Array<any> }) {
 
     return (
         <div className="">
-            <SearchInput />
+            <SearchInput inputValue={searchInputValue} handleKeyDown={handleSearchInputKeyDown} handleChange={handleSearchInputChange} />
             <CompactTable courses={filteredCourses} />
         </div>
     )
