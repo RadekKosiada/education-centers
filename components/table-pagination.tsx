93f10953566340efca7edc648687e15157480aed
RadekKosiada <kosiadar@gmail.com>
2026-03-26 19:21:53 +0100

    adjust href on PaginationLink

--- components/table-pagination.tsx
+++ components/table-pagination.tsx
@@ -7,6 +7,8 @@ import {
     PaginationLink
 } from "@/components/ui/pagination";
 import { rowsPerPage } from "@/lib/table-categories";
+import { useSearchParams } from "next/navigation";
+
 
 export function TablePagination({
     currentPage,
@@ -18,7 +20,11 @@ export function TablePagination({
     handlePageNumberChange: (pageNumber: string) => void
 }) {
     const numberOfPages = Math.ceil(coursesLength / rowsPerPage);
-    console.log(currentPage);
+
+    const searchParams = useSearchParams();
+    const searchQueryParam = searchParams.get('search') ?? null;
+    // const searchParams.get('page') ?? '1';
+
     return (
         <Pagination>
             <PaginationContent className="flex-wrap">
@@ -27,7 +33,9 @@ export function TablePagination({
                 </PaginationItem> */}
                 {Array.from({ length: numberOfPages }, (_, i) => (
                     <PaginationItem key={i} onClick={() => handlePageNumberChange((i + 1).toString())}>
-                        <PaginationLink href='#' isActive={currentPage === (i + 1).toString()}>
+                        <PaginationLink
+                            href={searchQueryParam ? `/?search=${searchQueryParam}&page=${i + 1}` : `page=${i + 1}`}
+                            isActive={currentPage === (i + 1).toString()}>
                             {i + 1}
                         </PaginationLink>
                     </PaginationItem>
